e70d89d6c29c96a5dbbceae59202683bac18d81c
Janos Perge <janos.perge@optimonk.com>
2023-11-29 12:47:29 +0100

    add prettier, jslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,15 +4,13 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="sass">
-
-</style>
+<style scoped lang="sass"></style>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: (config) => {
     // Pug Loader
     config.module
-      .rule("pug")
+      .rule('pug')
       .test(/\.pug$/)
-      .use("pug-plain-loader")
-      .loader("pug-plain-loader")
+      .use('pug-plain-loader')
+      .loader('pug-plain-loader')
       .end();
   },
 });
